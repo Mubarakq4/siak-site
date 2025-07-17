5f1c2e2f27d868658597bb6bb09bd95c285371c1
Mubarakq4 <mubaraknaik39@gmail.com>
2025-07-17 11:01:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,13 @@
-src/components/Footer.jsx
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  root: './src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  }
+})
